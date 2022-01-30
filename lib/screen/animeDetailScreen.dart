183b1b2d058e5f96e0a7ef0d3f0a3374066ee490
Candice Ariel <candicewangsa@gmail.com>
2022-01-30 18:28:41 +0700

    detail update

--- lib/screen/animeDetailScreen.dart
+++ lib/screen/animeDetailScreen.dart
@@ -217,7 +217,7 @@ class _animeDetailScreenState extends State<animeDetailScreen> {
                                                   ),
                                                   Flexible(
                                                     child: Container(
-                                                      padding: EdgeInsets.only(left: 10),
+                                                      padding: EdgeInsets.only(left: 20),
                                                       height: 50.0,
                                                       child:  ListView.builder(
                                                         scrollDirection: Axis.horizontal,
@@ -255,7 +255,7 @@ class _animeDetailScreenState extends State<animeDetailScreen> {
                                                   ),
                                                   Flexible(
                                                     child: Container(
-                                                      padding: EdgeInsets.only(left: 20),
+                                                      padding: EdgeInsets.only(left: 15),
                                                       height: 50.0,
                                                       child:  ListView.builder(
                                                         scrollDirection: Axis.horizontal,
